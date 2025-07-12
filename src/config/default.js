a20161414794bcd344b54d1d00cf71b846c9c2c2
Saranya-kirish25 <saranyakirishnamoorthy@gmail.com>
2025-07-12 19:18:11 +0530

    fix: config files converted to js

--- src/config/default.js
+++ src/config/default.js
@@ -1,8 +1,9 @@
-export default {
-  port: 1337,
+module.exports = {
+  port: process.env.PORT || 1337,
   dbUri: "",
   jwtSecret: "",
   saltRounds: 10,
   corsOrigin: "http://localhost:5173",
   host: "localhost",
 };
+
---
+++ src/config/production.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: process.env.PORT || 9000,
+  dbUri: "",
+  jwtSecret: "",
+  saltRounds: 10,
+};
+
