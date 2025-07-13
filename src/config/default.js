a55e0422149fee0a33e1f5bc77b217b64a991a2d
Saranya-kirish25 <saranyakirishnamoorthy@gmail.com>
2025-07-13 20:07:24 +0530

    fix: added missing type declarations for express and config

--- src/config/default.js
+++ src/config/default.js
@@ -1,11 +1,8 @@
-// config/default.js
 module.exports = {
-  port: 9000, // Or process.env.PORT || 9000
-  dbUri: "",
-  jwtSecret: "",
-  saltRounds: 10,
-  corsOrigin: "http://localhost:5173",
-  host: "localhost"
+  port: process.env.PORT || 5000,
+  jwtSecret: process.env.JWT_SECRET,
+  mongoURI: process.env.MONGODB_URI,
 };
+
 
 
