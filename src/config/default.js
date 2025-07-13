97b881e7e18deb2075c9c71f0f815c28fcac6bbd
Saranya-kirish25 <saranyakirishnamoorthy@gmail.com>
2025-07-13 21:26:54 +0530

    Fix: Added real config values

--- src/config/default.js
+++ src/config/default.js
@@ -1,8 +1,11 @@
+
+
 module.exports = {
-  port: process.env.PORT || 5000,
-  jwtSecret: process.env.JWT_SECRET,
-  mongoURI: process.env.MONGODB_URI,
+  port: 8080,
+  host: "0.0.0.0",
+  corsOrigin: "https://realtime-todo-app-front.vercel.app/projects", // உங்கள் frontend live URL
+  jwtSecret: "kanban_backend_project_@Saranya123!_secure_token", // 🔐 இங்க உங்கள் secret key
+  mongoURI:"mongodb+srv://saranyakirishnamoorthy:Saranya123@cluster0.s16unli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", // 🌐 MongoDB URI
+  saltRounds: 10
 };
-
-
 
